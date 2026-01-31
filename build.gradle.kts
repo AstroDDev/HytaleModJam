fd167ab76630978118dd1baccaaa38f7360eda39
Jeremy Karevich <cxomtdoh@yahoo.com>
2026-01-31 16:58:35 -0500

    Update gradle with direct server dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,19 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "hytale-release"
+        url = uri("https://maven.hytale.com/release")
+    }
+    maven {
+        name = "hytale-pre-release"
+        url = uri("https://maven.hytale.com/pre-release")
+    }
 }
 
 dependencies {
-    compileOnly(files("libs/HytaleServer.jar"))
+    compileOnly("com.hypixel.hytale:Server:2026.01.28-87d03be09")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
